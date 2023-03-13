ac7679bcf31160e9ab441a619005f4e0ce333706
muchiri08 <mbogokennedy08@gmail.com>
2023-03-13 21:04:08 +0300

    added method to add a task

--- todo.go
+++ todo.go
@@ -11,3 +11,14 @@ type item struct {
 	CompletedAt time.Time
 }
 type Todos []item
+
+func (t *Todos) Add(task string) {
+	todo := item{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Time{},
+	}
+
+	*t = append(*t, todo)
+}
