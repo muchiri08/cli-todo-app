04845a83d367e34f8260d1782b8b9a7dc48e5a58
muchiri08 <mbogokennedy08@gmail.com>
2023-03-14 21:40:04 +0300

    added flag delete

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -17,6 +17,8 @@ func main() {
 
 	complete := flag.Int("complete", 0, "mark a todo as complete")
 
+	delete := flag.Int("delete", 0, "deletes a todo from the file")
+
 	flag.Parse()
 
 	todos := &todo.Todos{}
@@ -37,6 +39,18 @@ func main() {
 		}
 	case *complete > 0:
 		err := todos.Complete(*complete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *delete > 0:
+		err := todos.Delete(*delete)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(1)
